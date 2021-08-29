387e8132d7af46c79d664d4a40c2a7505c5cf481
ESL7 <eslsilva13@gmail.com>
2021-08-29 15:23:58 -0400

    chore: setting up mongoose, dotenv and creating database connection

--- src/server.js
+++ src/server.js
@@ -1,8 +1,21 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+
 import routes from './router'
+
+dotenv.config()
 
 const server = express()
 const port = process.env.PORT || 3000
+
+mongoose.connect(
+  `mongodb+srv://ezequiel:${process.env.DB_PASSWORD}@cluster0.mxvx8.mongodb.net/${process.env.DB_NAME}`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+)
 
 server.use(express.json())
 server.use(routes)
